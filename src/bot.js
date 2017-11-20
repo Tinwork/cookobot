79febe8228a20d52e33f0b4b4cae01306eea9948
Antoine Renault <antoine.renault.mmi@gmail.com>
2017-11-20 18:20:13 +0100

    Initial commit for SLA chabot nameed cookobot

--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,9 @@
 const builder = require('botbuilder')
 const Promise = require('bluebird')
+const cognitiveServices = require('botbuilder-cognitiveservices')
 const request = require('request-promise').defaults({
   encoding: null
 })
-
-const baseUrl = ''
-
-console.log(process.env.MICROSOFT_APP_ID, process.env.MICROSOFT_APP_PASSWORD)
 
 const connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
@@ -15,30 +12,56 @@ const connector = new builder.ChatConnector({
 
 const bot = new builder.UniversalBot(connector)
 
-bot.endConversationAction('goodbyeAction', 'goodbyeAction', {
-  matches: /^goodbye|quit|bye/i
+const recogniser = new cognitiveServices.QnAMakerRecognizer({
+  knowledgeBaseId: process.env.KNOWLEDGE_BASE_ID,
+  subscriptionKey: process.env.SUBSCRIPTION_KEY
 })
 
-bot.dialog('/', [
-  session => {
-    if (session.userData.lang) {
-      session.beginDialog('/menu')
-    } else {
-     session.beginDialog('/menu')
-    }
-  }
-])
+const dialog = new cognitiveServices.QnAMakerDialog({
+  recognizers: [recogniser],
+  defaultMessage: 'Default message'
+})
 
+const luisEndpoint =
+  'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/35aa771f-ce0f-47b4-aa6f-dab385a8e620?subscription-key=7079e6989b494df4988560a8470cc3a9&spellCheck=true&verbose=true&timezoneOffset=0'
+const luisRecognizer = new builder.LuisRecognizer(luisEndpoint)
+bot.recognizer(luisRecognizer)
+
+//
+// DIALOGS DECLARATION
+//
+
+bot.dialog('/', dialog)
 bot
-  .dialog('/menu', [
-    session => {
-      session.send('Working')
-      
+  .dialog('HomePilot', [
+    (session, args, next) => {
+      const intent = args.intent
+
+      const DEFAULT_OBJECT = {
+        entity: 'Not found',
+        score: 0
+      }
+
+      const device = builder.EntityRecognizer.findEntity(intent.entities, 'HomeAutomation.Device') || DEFAULT_OBJECT
+      const operation = builder.EntityRecognizer.findEntity(intent.entities, 'HomeAutomation.Operation') || DEFAULT_OBJECT
+      const room = builder.EntityRecognizer.findEntity(intent.entities, 'HomeAutomation.Room') || DEFAULT_OBJECT
+
+      const data = `
+        Main intent : ${intent.intent} \n
+        Device : ${device.entity} ${device.score}
+        Operation : ${operation.entity} ${operation.score}
+        Room : ${room.entity} ${room.score}`
+
+      session.endDialog(data)
     }
   ])
-  .reloadAction('/menu', null, {
-    matches: /^menu|show menu|reload|list|start|restart/i
+  .triggerAction({
+    matches: 'HomeAutomation.TurnOn'
   })
+
+//
+// EXPORTS
+//
 
 module.exports = {
   connector,
